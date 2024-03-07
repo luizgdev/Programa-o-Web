363142fd94cd4cb40dbe478bca467139cf8c8a00
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2024-03-07 11:10:15 -0300

    Add files via upload

---
+++ tipo_var.js
@@ -0,0 +1,11 @@
+const j = 1 // Variável constante
+
+var n = 1 // Variável que pode ser ressignificada
+var n = "3"
+console.log(n)
+
+let i = 2 // Variável que não pode ser ressignificada
+i = 3
+console.log(i)
+
+// Uma variável que é declarada sem prefixo é tida como var pelo interpretador.
