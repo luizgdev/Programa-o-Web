ca9e04c2e1b96b8e7b4499a520f272491537f89a
LUIZ GUSTAVO DA COSTA GONÇALVES <luiz.ggoncalves@a.ucb.br>
2024-03-14 10:04:37 -0300

    Atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let var_a = 7, var_b = 5, resultado;
+
+console.log("Cálculos: ");
+
+resultado = var_a + var_b;
+console.log("soma: " + resultado);
+
+resultado = var_a - var_b;
+console.log("subtração: " + resultado);
+
+resultado = var_a * var_b;
+console.log("multiplicacao: " + resultado);
+
+resultado = var_a / var_b;
+console.log("divisao: " + resultado);
